e2d34c1c64a8c8b812175e5ebde247e5f6dea8ed
lapanikeugene <eugeny.lp@gmail.com>
2023-02-11 18:15:17 +0100

    complete project

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+let storage = multer.memoryStorage();
+let upload = multer({storage:storage});
+
+
 require('dotenv').config()
 
 var app = express();
@@ -11,7 +16,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  console.log(req.file,req.body)
+  return res.json({
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
